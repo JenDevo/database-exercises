4ffabb756234f1bb0673a14aced7180a6320a313
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-04-19 15:01:18 -0500

    Added all albums information to seeder

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,9 +5,9 @@ USE codeup_test_db;
 
     CREATE TABLE IF NOT EXISTS albums (
         id              INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        artist          VARCHAR(25) NOT NULL,
+        artist          VARCHAR(50) NOT NULL,
         name            VARCHAR(50) NOT NULL,
         release_date    INT UNSIGNED NOT NULL,
-        sales           FLOAT NOT NULL,
-        genre           VARCHAR(15) NOT NULL
+        sales           DOUBLE(5, 2) NOT NULL,
+        genre           VARCHAR(25) NOT NULL
     );
